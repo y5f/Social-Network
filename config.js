dba86b63cbce2803f0563c1b9c938535ccab8642
rsm2201 <rs42@hotmail.co.uk>
2016-07-18 12:16:01 +0100

    updated URI
    URI

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongo: {
-    uri: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/social'
+    uri: 'mongodb://heroku_bg3nfvzq:2sntsvjes2dt4jacr7nuklkopt@ds023485.mlab.com:23485/heroku_bg3nfvzq' || 'mongodb://127.0.0.1:27017/social'
   }
 }

4f93e28e6fa1142fb8d3789895e2765b713b0fc0
rsm2201 <rs42@hotmail.co.uk>
2016-07-19 11:22:35 +0100

    Usig mv instead of fs rename

--- server/controllers/profile-controller.js
+++ server/controllers/profile-controller.js
@@ -1,6 +1,7 @@
 var User = require('../datasets/users');
 var fs = require('fs-extra');
 var path = require('path');
+var mv = require('mv');
 
 module.exports.updatePhoto = function(req, res){
   var file = req.files.file;
@@ -16,7 +17,29 @@ module.exports.updatePhoto = function(req, res){
   var savePath = "/uploads/" + userId + uploadDate + file.name;
 
   //console.log("User: " + userId + " is submitting ", file);
+  mv(tempPath, targetPath, function(err) {
+    if (err) {
+      throw err;
+      res.json({status: 500})
+    } else {
+      User.findById(userId, function(err, userData){
+        var user = userData;
+        user.image = savePath;
+        user.save(function(err){
+          if(err){
+            console.log("failed save");
+            res.json({status: 500})
+          }else{
+            res.json({status: 200})
+            console.log('file moved successfully from ' + tempPath + ' to ' + targetPath);
+            console.log("save successful");
+          }
 
+        })
+      })  
+    }
+  });
+  /*
   fs.rename(tempPath, targetPath, function(err){
     if(err){
     console.log(err);
@@ -38,7 +61,7 @@ module.exports.updatePhoto = function(req, res){
     console.log("file moved");
   }
   })
-
+*/
 }
 
 module.exports.updateUsername = function(req, res){

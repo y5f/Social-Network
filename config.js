e8ae4d064b76c44b66041c2ae607aa3cffdcf22f
rsm2201 <rs42@hotmail.co.uk>
2016-07-28 18:30:31 +0100

    Environment Variables Fixed
    process env created using dotenv

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongo: {
-    uri: 'mongodb://heroku_bg3nfvzq:2sntsvjes2dt4jacr7nuklkopt@ds023485.mlab.com:23485/heroku_bg3nfvzq' || 'mongodb://127.0.0.1:27017/social'
+    uri: 'mongodb://127.0.0.1:27017/social' //'mongodb://heroku_bg3nfvzq:2sntsvjes2dt4jacr7nuklkopt@ds023485.mlab.com:23485/heroku_bg3nfvzq'
   },
     secret: 'bobsdevstuff'
 

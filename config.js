08621a88b0d1422219c5bcdaa608de250768ecee
rsm2201 <rs42@hotmail.co.uk>
2016-07-28 18:31:04 +0100

    process.env
    environment variables fixed

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongo: {
-    uri: 'mongodb://127.0.0.1:27017/social' //'mongodb://heroku_bg3nfvzq:2sntsvjes2dt4jacr7nuklkopt@ds023485.mlab.com:23485/heroku_bg3nfvzq'
-  },
-    secret: 'bobsdevstuff'
-
-}
